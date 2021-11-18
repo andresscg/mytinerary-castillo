4d5c1ced2a84f2ab0b43a85a7c720fe3ff473de7
andresscg <afelipecastillog@gmail.com>
2021-11-18 18:02:15 -0500

    Added assets and main components

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+
+const Navbar = () => {
+  return (
+    <div className="container">
+
+    </div>
+  )
+}
+
+export default Navbar
