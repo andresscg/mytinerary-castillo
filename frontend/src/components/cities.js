13e81afa3aeaa872744ec21bfa0fadb6f3a98018
andresscg <afelipecastillog@gmail.com>
2021-11-22 18:04:41 -0500

    Fixed problems with icons

---
+++ frontend/src/components/cities.js
@@ -0,0 +1,66 @@
+export const cities = [
+  [
+    {
+      name: "Bogota",
+      country: "Colombia",
+      img: "assets/bogota.jpg",
+    },
+    {
+      name: "New York",
+      country: "USA",
+      img: "assets/newyork.jpg",
+    },
+    {
+      name: "Paris",
+      country: "France",
+      img: "assets/paris.jpg",
+    },
+    {
+      name: "Munich",
+      country: "Germany",
+      img: "assets/munich.jpg",
+    },
+  ][
+    {
+      name: "Tokyo",
+      country: "Japan",
+      img: "assets/tokyo.jpg",
+    },
+    {
+      name: "Amsterdam",
+      country: "Netherlands",
+      img: "assets/amsterdam.jpg",
+    },
+    {
+      name: "Cairo",
+      country: "Egypt",
+      img: "assets/cairo.jpg",
+    },
+    {
+      name: "Cancun",
+      country: "Mexico",
+      img: "assets/cancun.jpg",
+    }
+  ][
+    {
+      name: "Victoria",
+      country: "Seychelles",
+      img: "assets/victoria.jpg",
+    },
+    {
+      name: "Sydney",
+      country: "Australia",
+      img: "assets/sydney.jpg",
+    },
+    {
+      name: "Istambul",
+      country: "Turkey",
+      img: "assets/istambul.jpg",
+    },
+    {
+      name: "Prague",
+      country: "Czech Republic",
+      img: "assets/prague.jpg",
+    }
+  ],
+];
