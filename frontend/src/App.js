aa5763c61ec0509e236e5715fc23727b876f976c
andresscg <afelipecastillog@gmail.com>
2021-11-22 23:36:47 -0500

    Deleted unused styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Cities from "./components/pages/Cities";
+import LogIn from './components/pages/LogIn'
+import SignUp from './components/pages/SignUp'
+
 import Footer from './components/Footer'
 
 
@@ -13,7 +16,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/" exact component={Cities}/>
+          <Route path="/cities" exact component={Cities}/>
+          <Route path="/login" exact component={LogIn}></Route>
+          <Route path="/signup" exact component={SignUp}></Route>
         </Switch>
         <Footer/>
       </Router>
