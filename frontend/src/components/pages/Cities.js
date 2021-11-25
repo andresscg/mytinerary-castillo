8da6a7949abac05402399ad6bf3d12e53aa52419
andresscg <afelipecastillog@gmail.com>
2021-11-25 16:18:12 -0500

    Created a node project and started a server which creates the api to be fetched on the fronted. Added City component and Cities page, displaying data from the server using hooks and fetch.

--- frontend/src/components/pages/Cities.js
+++ frontend/src/components/pages/Cities.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import City from "../City";
+import '../../App.css'
 
 const Cities = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [cities, setCities] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/api/cities")
+      .then((res) => res.json())
+      .then((data) => setCities(data.response.cities))
+      .catch((err) => console.error(err));
+  }, []);
 
-export default Cities
+  const filterCities = () => {
+    
+  }
+
+  return (
+    <div className="cities-container">
+      <video src="/assets/bg-video2.mp4" autoPlay loop muted></video>
+      <input type="text" placeholder="Search for a city " className="cities-input" />
+      <div className="cities-section">
+        {cities.map((city) => {
+          return (
+            <City key={`${city._id}`} name={city.name} country={city.country} img={city.img}/>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cities;
