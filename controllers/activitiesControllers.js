f7b3d6a054c40fb4695c5d4738d46c3b840d3a98
andresscg <afelipecastillog@gmail.com>
2022-01-03 11:26:23 -0500

    removed console.log

--- controllers/activitiesControllers.js
+++ controllers/activitiesControllers.js
@@ -2,7 +2,6 @@ const Activity = require('../models/Activity')
 
 const activitiesControllers = {
   addNewActivity: async(req, res) => {
-    console.log(req.body)
     const newActivity = new Activity({...req.body});
     try{
       await newActivity.save()
